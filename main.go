a688a738829976c6289432be896c66e91d1b610d
Paulo Borges <pborges475@gmail.com>
2016-07-04 16:57:15 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"github.com/pkg/term"
+	"os"
+	"fmt"
+	"strconv"
+	"net"
+	"io"
+	"sync"
+	"log"
+)
+
+var clients map[net.Conn]bool
+var writeLock *sync.Mutex
+
+func main() {
+	log.SetOutput(os.Stdout)
+
+	clients = make(map[net.Conn]bool)
+	writeLock = new(sync.Mutex)
+
+	if len(os.Args) != 4 {
+		fmt.Printf("usage: %s <serial port> <baud> <tcp port>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	serialPortName := os.Args[1]
+	baud, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Printf("Error parsing baud\n")
+		os.Exit(1)
+	}
+	tcpPort, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		fmt.Printf("Error parsing tcp port\n")
+		os.Exit(1)
+	}
+
+	serialPort, err := term.Open(serialPortName)
+	if err != nil {
+		fmt.Printf("Unable to open Serial Port %+v\n", err)
+		os.Exit(1)
+	}
+	serialPort.SetSpeed(baud)
+
+	l, err := net.Listen("tcp", ":" + strconv.Itoa(tcpPort))
+	if err != nil {
+		fmt.Printf("Unable to open Listen on Port %d %+v\n", tcpPort, err)
+		os.Exit(1)
+	}
+	defer l.Close()
+
+	go serialPortReader(serialPort)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		go handleRequest(conn, serialPort)
+	}
+}
+
+func serialPortReader(serialPort io.Reader) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := serialPort.Read(buf)
+		if err != nil {
+			fmt.Printf("Unable to read from serialport %+v\n", err)
+			os.Exit(1)
+		}
+		for k := range clients {
+			_, err = k.Write(buf[:n])
+			if err != nil {
+				fmt.Printf("Unable to write to conn %s %+v\n", k.RemoteAddr(), err)
+				os.Exit(1)
+			}
+			log.Printf("%d bytes -> %s\n", n, k.RemoteAddr())
+		}
+	}
+}
+
+func handleRequest(conn net.Conn, serialPort io.Writer) {
+	defer conn.Close()
+	defer delete(clients, conn)
+
+	buf := make([]byte, 1024)
+	clients[conn] = true
+
+	fmt.Printf("%d Connect %s\n", len(clients), conn.RemoteAddr())
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Printf("Unable to read from conn %s %+v\n", conn.RemoteAddr(), err)
+			return
+		}
+		writeLock.Lock()
+		serialPort.Write(buf[:n])
+		if err != nil {
+			fmt.Printf("Unable to read from conn %s %+v\n", conn.RemoteAddr(), err)
+			writeLock.Unlock()
+			return
+		}
+		writeLock.Unlock()
+		log.Printf("%s -> %d bytes\n", conn.RemoteAddr(), n)
+	}
+}
